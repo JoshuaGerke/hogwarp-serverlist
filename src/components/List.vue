e03c84c0337cf64844282b940b9a608cfc36a130
Joshua Gerke <kontakt@joshua-gerke.de>
2023-02-21 20:50:22 +0100

    fix: meta and favicon

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="max-w-7xl mx-auto h-screen">
-  <div class="h-[10rem]">
-    <div class=""></div>
+  <div class="mb-10 mt-20">
+    <div class="text-[#eed385] font-[Tongari] text-3xl text-center">Inofficial HogWarp Server List</div>
   </div>
   <div class="flex-wrap h-[76%] md:h-auto md:flex">
     <div class="mx-auto w-11/12 md:w-9/12 mb-auto h-[75%]">
