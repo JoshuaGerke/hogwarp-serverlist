50b871b84fe1fb9796dc11d4769c97b6ce528ac8
Joshua Gerke <kontakt@joshua-gerke.de>
2023-02-21 20:17:55 +0100

    feat: renewd

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,33 +1,45 @@
 <template>
-<div class="">
-  <div class="">
+<div class="max-w-7xl mx-auto h-screen">
+  <div class="h-[10rem]">
     <div class=""></div>
   </div>
-  <div class="h-[10rem] bg-btn flex flex-col items-center justify-center align-middle flex-wrap" style="background-size: 100% 100%">
-    <div class="mx-auto w-full text-[#a06738] text-4xl font-normal text-center uppercase font-bold">Serverlist</div>
-    <div class="mx-auto w-full text-[#a06738] text-4xl font-normal text-center uppercase font-semibold">Online Servers <b>{{servers.length}}</b> with <b>{{getAllPlayers()}}</b> Players</div>
-  </div>
-  <div class="flex">
-    <div class="flex flex-wrap w-1/2 items-center justify-center content-center mx-auto">
-      <div class="flex w-full bg-btn p-2 text-[#a06738] mb-4 uppercase font-semibold" style="background-size: 100% 100%">
-        <div class="w-1/12"></div>
-        <div class="w-6/12">Name</div>
-        <div class="w-1/12">Players</div>
-        <div class="w-2/12">Version</div>
-        <div class="w-2/12">Tags</div>
-      </div>
-      <div class="overflow-auto w-full h-[45rem]">
-      <div v-for="server in sortArr(servers)" style="background-size: 100% 100%" class="flex relative w-full bg-btn bg-contain bg-no-repeat p-2 py-4 mb-4 text-[#a06738] text-xl font-normal items-center">
-        <div class="w-1/12"><img v-if="server.icon_url" class="w-10 mx-auto object-contain object-center h-10" :src="server.icon_url"/></div>
-        <div class="w-6/12"><div class="">{{server.name}}</div><div class="text-sm">{{server.ip}}</div></div>
-        <div class="w-1/12">{{server.player_count}}/{{server.max_player_count}}</div>
-        <div class="w-2/12">{{server.version}}</div>
-        <div class="w-2/12">{{server.tags}}</div>
-      </div>
+  <div class="flex-wrap h-[76%] md:h-auto md:flex">
+    <div class="mx-auto w-11/12 md:w-9/12 mb-auto h-[75%]">
+      <div class="w-full flex flex-wrap items-center h-full content-start">
+        <div class="flex w-full h-12 bg-btn mb-2 items-center text-[#eed385] font-[Tongari] font-semibold" style="background-size: 100% 100%;">
+          <div class="w-1/12"></div>
+          <div class="w-6/12 mb-2">Name</div>
+          <div class="w-2/12 mb-2">Players</div>
+          <div class="w-2/12 mb-2">Version</div>
+          <div class="w-2/12 mb-2">Language</div>
+        </div>
+        <div class="w-full flex flex-wrap items-center overflow-auto h-[80%] md:h-[40rem] content-start">
+        <div v-for="server in sortArr(servers)" class="flex w-full h-14 bg-btn mb-2 items-center " style="background-size: 100% 100%">
+          <div class="w-1/12"><img v-if="server.icon_url" @error="imageUrlAlt" class="mx-auto w-8 h-8" :src="server.icon_url"/>
+          <div class="text-[#eed385] font-[Tongari] text-3xl text-center">{{!server.icon_url ? '?' : ''}}</div></div>
+          <div class="w-6/12 text-[#eed385] font-[Tongari] mb-2 flex flex-wrap">
+            <div class="block md:hidden w-full -mb-1">{{server.name.length >= 30 ? server.name.substring(0, 27) + '...' : server.name.substring(0, 29)}}</div>
+            <div class="hidden md:block w-full -mb-1">{{server.name.length >= 45 ? server.name.substring(0, 43) + '...' : server.name.substring(0, 45)}}</div><div @click="copyURL(server.ip + ':' + server.port)" class="cursor-pointer w-max text-sm">{{server.ip}}:{{server.port}}</div>
+          </div>
+          <div class="w-2/12 text-[#eed385] font-[Tongari] mb-2">{{server.player_count}}/{{server.max_player_count}}</div>
+          <div class="w-2/12 text-[#eed385] font-[Tongari] mb-2">{{server.version}}</div>
+          <div class="w-2/12 text-[#eed385] font-[Tongari] mb-2 flex gap-2"><div :class="`fi fi-${server.tags.toLowerCase()}`"></div><div class="">{{getCountryName(server.tags)}}</div></div>
+        </div>
+        </div>
       </div>
     </div>
+    <div class="w-11/12 mx-auto md:w-3/12">
+        <div class="max-w-[20rem] md:max-w-none mx-auto w-full md:ml-20 flex md:flex-wrap items-center">
+          <div class="mx-auto w-1/2 md:w-full"><img class="w-[8rem] md:w-full" :src="'img/bird.png'"></div>
+            <div class="w-1/2 md:w-full flex flex-wrap">
+              <div class="uppercase md:text-center text-[#eed385] font-[Tongari] w-auto md:w-full">Servers: <b class="text-xl">{{servers.length}}</b></div>
+              <div class="uppercase md:text-center text-[#eed385] font-[Tongari] w-auto md:w-full">Online Players: <b class="text-xl">{{getAllPlayers()}}</b></div>
+            </div>
+
+        </div>
+    </div>
   </div>
-  <div class="fixed bottom-4 w-full">
+  <div class="fixed left-0 right-0 bottom-8 text-[#eed385] font-[Tongari] opacity-50 w-full">
     <a class="mx-auto w-full text-[#a06738] text-md font-normal text-center uppercase font-normal block" target="_blank" href="https://joshua-gerke.de/imprint">Imprint</a>
   </div>
 </div>
@@ -35,8 +47,10 @@
 
 <script>
 import { defineComponent } from 'vue'
+import "flag-icons/css/flag-icons.min.css";
 export default defineComponent({
   name: 'List',
+  components: {},
   data () {
     return {
       servers: []
@@ -46,6 +60,18 @@ export default defineComponent({
     return {}
   },
   methods: {
+    async copyURL(mytext) {
+      try {
+        await navigator.clipboard.writeText(mytext);
+      } catch($e) {
+      }
+    },
+    getCountryName(countryCode) {
+      return new Intl.DisplayNames(['en'], {type: 'region'}).of(countryCode);
+    },
+    imageUrlAlt(img) {
+      img.target.style.display = "none";
+    },
     getAllPlayers() {
       let players = 0;
       this.servers.forEach(server => {
@@ -68,10 +94,9 @@ export default defineComponent({
       return fetch('https://list.hogwarp.com/list')
         .then((response) => response.json())
         .then((json) => {
-          console.log(json);
           this.servers = json.servers;
         }).catch((error) => {
-          this.servers = [];
+          this.servers = [{"desc":"Test","icon_url":"","ip":"???.???.???.???","max_player_count":'?',"name":"?????????","pass":false,"player_count":'?',"port":'???',"public":true,"tags":"","tick":60,"version":"???"}];
         });
     },
   },
@@ -94,25 +119,34 @@ body::after {
   right: 0;
   top: 0;
   bottom: 0;
-  background: url("img/bg.png");
+  background: url("img/bgmobile.png");
   background-size: 100% 100%;
+}
+
+@media screen and (min-width: 768px) {
+  body::after {
+    background: url("img/bg.png");
+    background-size: 100% 100%;
+  }
+
 }
 
 /* width */
 ::-webkit-scrollbar {
-  width: 10px;
+  width: 5px;
 }
 
 /* Track */
 ::-webkit-scrollbar-track {
-  background: #231524;
+  background: #1a1212;
   border-radius: 2rem;
 }
 
 /* Handle */
 ::-webkit-scrollbar-thumb {
-  background: #a06738;
-  border-radius: 2rem;
+  background: #ecd184;
+  border-radius: 0rem;
+  width: 0.2rem;
 }
 
 /* Handle on hover */
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,12 @@ module.exports = {
     extend: {
       backgroundImage: {
         "btn": "url('img/button.png')",
+        "desktopbg": "url('img/bg.png')",
+        "mobilebg": "url('img/bgmobile.png')",
+      },
+      colors: {
+        "baseFont": "rgb(255, 228, 184)",
+        "baseFontHover": "rgb(255, 205, 107)",
       }
     },
   },
