f055a5dc0a7921691aabd1ebe230a10e52bf5776
Joshua Gerke <kontakt@joshua-gerke.de>
2023-02-22 01:15:31 +0100

    feat: fixed build

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,8 +23,6 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
     <link rel="manifest" href="/favicon/site.webmanifest">
     <title>HogWarp Server List</title>
-
-    @vite('resources/css/app.css')
 </head>
 <body>
 <div id="app"></div>
