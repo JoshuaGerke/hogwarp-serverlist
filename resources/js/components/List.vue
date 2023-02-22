7253cf730060348663988e784f38cf4a51d2de62
Joshua Gerke <kontakt@joshua-gerke.de>
2023-02-22 01:28:04 +0100

    feat: fixed build

--- resources/js/components/List.vue
+++ resources/js/components/List.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="max-w-7xl mx-auto h-screen">
-        <div class="mb-10 mt-20">
-            <div class="text-[#eed385] font-[Tongari] text-3xl text-center">Inofficial HogWarp Server List</div>
+        <div class="mb-10 mt-[6rem]">
+            <div class="text-[#eed385] font-[Tongari] text-3xl font-bold text-center">Inofficial HogWarp Server List</div>
         </div>
         <div class="flex-wrap h-[76%] md:h-auto md:flex">
             <div class="mx-auto w-11/12 md:w-9/12 mb-auto h-[75%]">
@@ -32,8 +32,8 @@
                 <div class="max-w-[20rem] md:max-w-none mx-auto w-full md:ml-20 flex md:flex-wrap items-center">
                     <div class="mx-auto w-1/2 md:w-full"><img class="w-[8rem] md:w-full" :src="'img/bird.png'"></div>
                     <div class="w-1/2 md:w-full flex flex-wrap">
-                        <div class="uppercase md:text-center text-[#eed385] font-[Tongari] w-auto md:w-full">Servers: <b class="text-xl">{{servers.length}}</b></div>
-                        <div class="uppercase md:text-center text-[#eed385] font-[Tongari] w-auto md:w-full">Online Players: <b class="text-xl">{{getAllPlayers()}}</b></div>
+                        <div class="uppercase md:text-center text-2xl text-[#eed385] font-[Tongari] w-auto md:w-full">Servers: <b class="text-3xl">{{servers.length}}</b></div>
+                        <div class="uppercase md:text-center text-2xl text-[#eed385] font-[Tongari] w-auto md:w-full">Online Players: <b class="text-3xl">{{getAllPlayers()}}</b></div>
                     </div>
 
                 </div>
